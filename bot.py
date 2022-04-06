9b643275a0769c3b234e8240f37ce44e63b2fe59
Aidan <admin@aidanlemay.com>
2022-04-06 13:19:33 +0000

    Bot Updates

--- bot.py
+++ bot.py
@@ -9,6 +9,9 @@ from discord_slash.utils.manage_commands import create_option
 import subprocess
 import storage
 import feedparser
+import json
+import requests
+from requests.exceptions import HTTPError
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
@@ -20,6 +23,39 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
+bot.remove_command('help')
+
+@bot.command()
+async def help(ctx):
+    """Gets Status of RPI Server"""
+    await ctx.send("```\nRaspberryPiBot Discord Bot Help!\n\nCreated by Aidan LeMay using Discord.py\nhttps://github.com/The-Doctor-Of-11/RaspberryPiBot\n\n__Command Help:__\n/help: Display this help window\n/status or /stat: show status of Raspberry Pi Server\n/M911 [X#: Optional Quantity]: Returns X# of Monroe County 911 Events from https://www.monroecounty.gov/incidents911.rss\n\nVisit the creator here! https://aidanlemay.com/```")
+
+@bot.command()
+async def cronStat(ctx):
+    """Gets status of currently monitored CRON job"""
+    cron = subprocess.run(["curl https://cronitor.io/api/monitors -u " + storage.cronShork + ":"], shell=True, stdout=subprocess.PIPE).stdout.decode('utf8')
+    jcron = json.loads(cron)
+    jcron = jcron['monitors']
+    jcron = json.loads(jcron)
+
+    # jobName = jcron["monitors"]["name"]
+    # status = jcron["monitors"]["passing"]
+    # interval = jcron["monitors"]["schedule"]
+
+    # try:
+    #     jc = subprocess.run(["curl https://cronitor.io/api/monitors -u " + storage.cronShork + ":"], shell=True, stdout=subprocess.PIPE).stdout.decode('utf8')
+    #     response = json.loads(jc)
+    #     jsonResponse = response.json()
+    #     print(jsonResponse)
+
+    # except HTTPError as http_err:
+    #     print(f'HTTP error occurred: {http_err}')
+    # except Exception as err:
+    #     print(f'Other error occurred: {err}')
+
+    # await ctx.send("```\n Current Cron Status for " + jobName + ": \n\n" + status + "\n Run Schedule: " + interval + " \n```")
+    await ctx.send("```\n" + jcron['name'] + "\n```")
 
 @bot.command()
 async def status(ctx):
@@ -49,7 +85,7 @@ async def stat(ctx):
 @bot.command()
 async def M911(ctx, num=1):
 
-    d = feedparser.parse("https://www.monroecounty.gov/911/rss.php")
+    d = feedparser.parse("https://www.monroecounty.gov/incidents911.rss")
     entry = d.entries[num]
 
     i = 0
@@ -59,5 +95,23 @@ async def M911(ctx, num=1):
         response += "\n---------- Description -------------\n\n" + d['entries'][i]['summary'] + "\n```"
         i += 1
         await ctx.send(response)
+
+@bot.command()
+async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+    """A global error handler cog."""
+
+    if isinstance(error, commands.CommandNotFound):
+        message = "Sorry, this command was not found. Please check your input and try again!"
+    elif isinstance(error, commands.CommandOnCooldown):
+        message = f"This command is on cooldown. Please try again after {round(error.retry_after, 1)} seconds."
+    elif isinstance(error, commands.MissingPermissions):
+        message = "You are missing the required permissions to run this command!"
+    elif isinstance(error, commands.UserInputError):
+        message = "Something about your input was wrong, please check your input and try again!"
+    else:
+        message = "Oh no! Something went wrong while running the command!"
+
+    await ctx.send(message, delete_after=5)
+    await ctx.message.delete(delay=5)
 
 bot.run(storage.tstore)
